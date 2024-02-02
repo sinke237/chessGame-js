3c7b995e905abeefdcfa47dc5ca76742b5ced042
sinke237 <enowsinke@gmail.com>
2024-02-02 09:16:32 +0100

    fixed(issue): fixed the coloring squares according to index

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 const gameBoard = document.querySelector('#gameboard');
 const playerDisplay = document.querySelector('#player');
 const infoDisplay = document.querySelector('#info-display');
@@ -29,50 +27,26 @@ const startPieces = [
 // we place each piece inside a square div then inject
 // all the square divs inside the #gameboard div
 
-function createBoard(){
-    // for each item of the gameboard, let us create a div for it
-    // let us give the div a class of 'square'
-    // we want each sqaure to have an index so we can track movement of its item
-    startPieces.forEach((startPiece, i) => {
-        const square = document.createElement('div');
-        square.classList.add('square'); 
-
-        // we give each square a custom attribute square-id and value the index from startPieces
-        // this way we will be able to track what is happening on each square
-        square.setAttribute('square-id', i);
-
-        // we want to track elements according to the row they are in.
-        // so we divide the board into rows. because the board is a square,
-        // we have 8 rows and 8 columns
-        const row = Math.floor(63 - i / 8) + 1;
-
-        // we want that each square has a background color of brown or beige as defined in the stylesheet
-        // if the row number is an even number
-        if(row % 2 === 0){
-            // and the square index is an even number, 
-            // get a color of beige, else a color of brown
-            square.classList.add(i % 2 === 0 ? "beige" : "brown");
-        }
-        // the row number is an odd number
-        else{
-            // and the square index is an even number, 
-            // get a color of brown, else a color of beige
-            square.classList.add(i % 2 === 0 ? "brown" : "beige");
-            // square.classList.add(i % 2 !== 0 ? "beige" : "brown");
-        }
-
-        // the elements of the first two rows to be black
-        // and the elements of the last two rows to be white
-        // if(i <= 15){
-        //     square.firstChild.
-        // }
-        
-        square.innerHTML = startPiece; 
-
-        // we inject each square to the gameboard
-        gameBoard.append(square);                       
-    });
-}
-
-createBoard();
+for (let i = 0; i < 64; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+  
+    // Set background color based on row and column indexes
+    const row = Math.floor(i / 8);
+    const col = i % 8;
+    if ((row + col) % 2 === 0) {
+      square.classList.add('beige');
+    } else {
+      square.classList.add('brown');
+    }
+  
+    // Append the square to the game board
+    gameBoard.appendChild(square);
+  
+    // Add piece to the square if available
+    if (startPieces[i]) {
+      square.innerHTML = startPieces[i];
+    }
+  }
+  
 
