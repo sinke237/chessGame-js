77fb90ef6f06946f06ec41143e6b01ab3fe4b170
sinke237 <enowsinke@gmail.com>
2024-02-03 20:57:23 +0100

    fxn(dragOver): initializing dragOver event listener

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const infoDisplay = document.querySelector('#info-display');
 // where is each piece located. this is what we try to define now
 
 const width = 8;
+
+// We need to track who is playing, is it the white or black player?
+let playerGo = 'black'; // we set it to black by default.
+
+// we let the users know whose turn to player
+playerDisplay.textContent = 'black';
+
 const startPieces = [
     rook, knight, bishop, queen, king, bishop, knight, rook,
     pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
@@ -61,6 +68,9 @@ allSquares.forEach(square => {
     // When we to track the behaviour of elements that the dragged div 
     // passes over 
     square.addEventListener('dragover', dragOver);
+
+    // we also track where the square element is dropped
+    square.addEventListener('drop', dragDrop);
 });
 
 // elements to help us track drag and drop functionality
@@ -73,11 +83,26 @@ function dragStart(e) {
     startPositionID = e.target.parentNode.getAttribute('square-id');
     
     // this tracks the div that is being dragged
-    draggedElement = e.target;
-    
+    draggedElement = e.target;    
 }
 
 // prevent misbehaviour during dragover event
 function dragOver(e) {
     e.preventDefault();
+}
+
+// we will be dropping into squares that have something or are empty
+// we want to make sure we drop into the square not the svg
+function dragDrop(e) {
+    e.stopPropagation(); // to prevet misbehaviour
+
+    // we have to know that square that already has a piece in it.
+    const taken = e.target.classList.contains('piece');
+
+    // our function to change player
+    changePlayer();
+}
+
+function changePlayer() {
+    
 }
